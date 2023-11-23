7d3326c3b217278eba8beea3a8139efc6d23f8b6
dummyx <40601688+dummyx@users.noreply.github.com>
2023-11-23 12:12:05 +0900

    fix

--- tools/converter.py
+++ tools/converter.py
@@ -7,21 +7,24 @@ def check_integrity():
     pass
 
 def convert_image(path: str):
-    with open(path, "rb") as f:
-        image_file_bytes = f.read()
+    try:
+        with open(path, "rb") as f:
+            image_file_bytes = f.read()
 
+        if len(image_file_bytes) == 0:
+            return
+        frame_length = unpack("H", image_file_bytes[2: 4])[0]
+        payload = image_file_bytes[20:-2]
 
-    frame_length = unpack("H", image_file_bytes[2: 4])[0]
-    payload = image_file_bytes[20:-2]
-
-    frame_width = int(image_file_bytes[14])
-    frame_height = int(image_file_bytes[15])
-    with open(path + '.png', 'wb') as f: 
-        f = open(path + '.png', 'wb')      # binary mode is important
-        w = png.Writer(frame_width, frame_height, greyscale=True)
-        w.write(f, [ payload[i*frame_width : (i+1)*frame_width]  for i in range(frame_height)])
-        f.close()
-
+        frame_width = int(image_file_bytes[14])
+        frame_height = int(image_file_bytes[15])
+        with open(path + '.png', 'wb') as f: 
+            f = open(path + '.png', 'wb')      # binary mode is important
+            w = png.Writer(frame_width, frame_height, greyscale=True)
+            w.write(f, [ payload[i*frame_width : (i+1)*frame_width]  for i in range(frame_height)])
+            f.close()
+    except Exception as e:
+        pass
 if __name__ == "__main__":
     path = sys.argv[1]
 
